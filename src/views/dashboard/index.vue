a625881fa0e0abd5136669b4efc98820825cdf8b
mr.zhao <120220275@qq.com>
2019-11-19 23:45:50 +0800

    init box wrap

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">Something...</div>
+    <div class="boxWrap">
+      <div class="horn">
+        <div class="lt"></div>
+        <div class="rt"></div>
+        <div class="rm"></div>
+        <div class="rb"></div>
+        <div class="lb"></div>
+        <div class="lm"></div>
+      </div>
+      <div class="title-text">井盖设备告警</div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(["name"])
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -25,6 +34,58 @@ export default {
   &-text {
     font-size: 30px;
     line-height: 46px;
+  }
+}
+
+.boxWrap {
+  width: 555px;
+  height: 255px;
+  position: absolute;
+  background-color: #00d4e705;
+  .horn {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: 1px solid rgb(82, 82, 82);
+    > div {
+      width: 25px;
+      height: 25px;
+      position: absolute;
+    }
+    .lt {
+      border-top: 4px solid #00d3e7;
+      border-left: 4px solid #00d3e7;
+      left: -3px;
+      top: -3px;
+    }
+    .rt {
+      border-top: 4px solid #00d3e7;
+      border-right: 4px solid #00d3e7;
+      right: -3px;
+      top: -3px;
+    }
+    .rm {
+      border-right: 4px solid #00d3e7;
+      right: -3px;
+      top: calc(50% - 14.5px);
+    }
+    .rb {
+      border-bottom: 4px solid #00d3e7;
+      border-right: 4px solid #00d3e7;
+      right: -3px;
+      bottom: -3px;
+    }
+    .lb {
+      border-bottom: 4px solid #00d3e7;
+      border-left: 4px solid #00d3e7;
+      left: -3px;
+      bottom: -3px;
+    }
+    .lm {
+      border-left: 4px solid #00d3e7;
+      left: -3px;
+      top: calc(50% - 14.5px);
+    }
   }
 }
 </style>
