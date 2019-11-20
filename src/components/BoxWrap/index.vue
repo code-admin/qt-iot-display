ca1be40e1a0ad237aa58b55eb3fe4385cf6f569e
mr.zhao <120220275@qq.com>
2019-11-21 00:15:53 +0800

    add card layout...

--- src/components/BoxWrap/index.vue
+++ src/components/BoxWrap/index.vue
@@ -8,9 +8,17 @@
       <div class="lb" />
       <div class="lm" />
     </div>
-    <div class="title-text">
-      <span>{{ title }}</span>
-      <span>{{ subtext }}</span>
+    <div class="title-context">
+      <div class="lt" />
+      <div class="rt" />
+      <div class="rm" />
+      <div class="rb" />
+      <div class="lb" />
+      <div class="lm" />
+      <div class="title">
+        <span>{{ title }}</span>
+        <span>{{ subtext }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -36,13 +44,13 @@ export default {
   width: 555px;
   height: 255px;
   position: absolute;
-  background-color: #00d4e705;
+  background-color: #031d2e79;
 
   .horn {
     position: absolute;
     width: 100%;
     height: 100%;
-    border: 1px solid rgb(82, 82, 82);
+    border: 3px double #476479;
     > div {
       width: 25px;
       height: 25px;
@@ -81,6 +89,70 @@ export default {
       border-left: 4px solid #00d3e7;
       left: -3px;
       top: calc(50% - 14.5px);
+    }
+  }
+
+  .title-context {
+    width: 265px;
+    height: 40px;
+    left: 50%;
+    top: 0;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    background-color: #011122;
+    border: 2px solid #354a59;
+    > div {
+      width: 8px;
+      height: 8px;
+      position: absolute;
+    }
+    .lt {
+      border-top: 3px solid #00d3e7;
+      border-left: 3px solid #00d3e7;
+      left: -3px;
+      top: -3px;
+    }
+    .rt {
+      border-top: 3px solid #00d3e7;
+      border-right: 3px solid #00d3e7;
+      right: -3px;
+      top: -3px;
+    }
+    .rm {
+      border-right: 3px solid #00d3e7;
+      right: -3px;
+      top: calc(50% - 4px);
+    }
+    .rb {
+      border-bottom: 3px solid #00d3e7;
+      border-right: 3px solid #00d3e7;
+      right: -3px;
+      bottom: -3px;
+    }
+    .lb {
+      border-bottom: 3px solid #00d3e7;
+      border-left: 3px solid #00d3e7;
+      left: -3px;
+      bottom: -3px;
+    }
+    .lm {
+      border-left: 3px solid #00d3e7;
+      left: -3px;
+      top: calc(50% - 4px);
+    }
+
+    .title {
+      width: 100%;
+      height: 100%;
+      font-size: 20px;
+      line-height: 40px;
+      text-align: center;
+      font-weight: bold;
+      letter-spacing: 2px;
+      text-shadow: 0 0 25px #fcf58e;
+      span:first-of-type {
+        color: #fcf58e;
+      }
     }
   }
 }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="dashboard-container">
-    <BoxWrap title="井盖" subtext="设备告警" />
+    <BoxWrap class="clazz01" title="井盖" subtext="设备告警" />
+
+    <BoxWrap class="clazz02" title="井盖" subtext="告警信息" />
+
+    <BoxWrap class="clazz03" title="雨水井" subtext="设备告警" />
+
+    <BoxWrap class="clazz04" title="雨水井" subtext="告警信息" />
+
+    <BoxWrap class="clazz05" title="污水井" subtext="设备告警" />
+
+    <BoxWrap class="clazz06" title="污水井" subtext="告警信息" />
   </div>
 </template>
 
@@ -23,10 +33,39 @@ export default {
 .dashboard {
   &-container {
     margin: 30px;
+    position: relative;
   }
   &-text {
     font-size: 30px;
     line-height: 46px;
   }
+}
+
+.clazz01 {
+  left: 10px;
+}
+
+.clazz02 {
+  right: 10px;
+}
+
+.clazz03 {
+  top: 450px;
+  left: 10px;
+}
+
+.clazz04 {
+  right: 10px;
+  top: 450px;
+}
+
+.clazz05 {
+  top: 750px;
+  left: 10px;
+}
+
+.clazz06 {
+  right: 10px;
+  top: 750px;
 }
 </style>
