06a86f36a4c413eff9d4a3a6333deb9404208751
mr.zhao <120220275@qq.com>
2019-12-15 23:28:16 +0800

    upgrade login page style.

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <div class="title-container">
+      <h3 class="title">欢迎使用</h3>
+      <h3 class="title">智能设备综合管理平台</h3>
+    </div>
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" inline class="login-form" auto-complete="on" label-position="left">
+
+      <div class="subtitle">
+        用户登录
       </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
+      <el-form-item prop="username" label="登录名：">
+        <!-- <span class="svg-container">
           <svg-icon icon-class="user" />
-        </span>
+        </span> -->
         <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text" tabindex="1" auto-complete="on" />
       </el-form-item>
 
-      <el-form-item prop="password">
-        <span class="svg-container">
+      <el-form-item prop="password" label="密  码：">
+        <!-- <span class="svg-container">
           <svg-icon icon-class="password" />
-        </span>
+        </span> -->
         <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="Password" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
         <span class="show-pwd" @click="showPwd">
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <div class="tips">
+      <!-- <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: any</span>
-      </div>
+      </div> -->
 
     </el-form>
   </div>
@@ -156,12 +160,42 @@ $cursor: #fff;
             }
         }
     }
-
+    .el-button--primary {
+      height:60px;
+      font-size:30px;
+      border-radius: 0;
+      background-color: #00f3fb81;
+      border-color: #00f3fb81;
+      span {
+        letter-spacing: 30px;
+      }
+    }
     .el-form-item {
-        border: 1px solid rgba(255, 255, 255, 0.1);
-        background: rgba(0, 0, 0, 0.1);
+        // background: rgba(0, 0, 0, 0.1);
         border-radius: 5px;
         color: #454545;
+        margin-bottom: 40px;
+
+        &__content {
+          width: 395px;
+          background: url(../../assets/inline.png) no-repeat;
+          background-size: cover
+        }
+
+        &__label {
+          width:139px;
+          height:26px;
+          font-size: 22px;
+          line-height: 47px;
+          font-weight: bold;
+          letter-spacing: 4px;
+          color:rgba(255,255,255,1);
+          &::before {
+            margin-right: 8px;
+            line-height: 47px;
+            vertical-align: sub;
+          }
+        }
     }
 }
 </style>
@@ -176,14 +210,29 @@ $light_gray:#eee;
     width: 100%;
     background-color: $bg;
     overflow: hidden;
+    background: url(../../assets/loginbg.jpg) no-repeat;
+    background-size: cover;
 
     .login-form {
         position: relative;
-        width: 520px;
+        width: 845px;
         max-width: 100%;
-        padding: 160px 35px 0;
+        height: 550px;
+        padding: 100px 150px;
         margin: 0 auto;
         overflow: hidden;
+        background: url(../../assets/loginformbg.png) center no-repeat;
+        background-size: contain;
+    }
+
+    .subtitle {
+      height:37px;
+      font-size:38px;
+      font-weight: bold;
+      text-align: center;
+      margin-bottom: 55px;
+      letter-spacing: 6px;
+      color:rgba(255,255,255,1);
     }
 
     .tips {
@@ -208,13 +257,22 @@ $light_gray:#eee;
 
     .title-container {
         position: relative;
-
+        text-align: center;
+        margin-top: 135px;
         .title {
-            font-size: 26px;
-            color: $light_gray;
-            margin: 0px auto 40px auto;
-            text-align: center;
-            font-weight: bold;
+            opacity:0.8;
+            width: 800px;
+            height: 75px;
+            font-size:72px;
+            line-height: 1;
+            font-weight:bold;
+            margin: 60px auto 0;
+            letter-spacing: 5px;
+            color:rgba(255,255,255,1);
+            background: linear-gradient(0deg,rgba(85,189,243,1) 0%, rgba(255,255,255,1) 100%);
+            background-clip:text;
+            -webkit-background-clip:text;
+            -webkit-text-fill-color:transparent;
         }
     }
 
