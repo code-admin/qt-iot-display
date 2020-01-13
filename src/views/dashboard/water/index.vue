eece11536c2c94d8a12f62e9d904e9d9224afcc8
mr.zhao <120220275@qq.com>
2020-01-14 00:56:59 +0800

    update scrollData method

--- src/views/dashboard/water/index.vue
+++ src/views/dashboard/water/index.vue
@@ -188,24 +188,32 @@ export default {
       getAlarmList({ pageIndex: 1, pageSize: 10 }).then(res => {
         if (res.code === 10000) {
           this.tableData = res.data;
-          this.scrollData('.wrap-table', this.tableData);
+          setInterval(() => this.scrollData('.wrap-table', this.tableData), 4000);
         }
       });
     },
     scrollData: function(tableClass, tableData) {
+      const child = 'tr.el-table__row:first-of-type';
       const selector = tableClass + ' .el-table__body-wrapper';
       const scrollElement = document.querySelectorAll(selector);
       if (scrollElement.length && tableData.length > 4) {
         const removeItem = tableData.splice(-1, 1);
-        scrollElement[0].style.transition = 'all .8s';
-        scrollElement[0].style.marginTop = '44px';
+        scrollElement[0].style.transition = 'all 0s';
+        scrollElement[0].style.marginTop = '-44px';
+        tableData.unshift(removeItem[0]);
+
+        const firstRow = scrollElement[0].querySelectorAll(child);
+        firstRow[0].style.transition = 'all 0s';
+        firstRow[0].style.opacity = '0';
 
         setTimeout(() => {
-          tableData.unshift(removeItem[0]);
-          scrollElement[0].style.transition = 'all 0s ease 0s';
-          scrollElement[0].style.marginTop = '0';
-        }, 800);
-        setTimeout(() => this.scrollData(tableClass, tableData), 4000);
+          scrollElement[0].style.transition = 'all .8s';
+          scrollElement[0].style.marginTop = '0px';
+
+          firstRow[0].style.transition = 'all 2.8s';
+          firstRow[0].style.opacity = '1';
+        }, 200);
+        // setTimeout(() => this.scrollData(tableClass, tableData), 4000);
       }
     },
     // 实时水位变化
